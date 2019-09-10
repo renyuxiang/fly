f8b6c410fa29e3b1e1d0f8af43376c57099e7944
renyx <1079010282@qq.com>
2019-09-10 13:52:28 +0800

    tfidf、csv

--- util/file_deal/csv_deal.py
+++ util/file_deal/csv_deal.py
@@ -5,7 +5,7 @@ import csv
 
 
 def read_csv():
-    r_path = './csv_example.csv'
+    r_path = '../data/csv_example.csv'
     csv_file=csv.reader(open(r_path,'r', encoding='utf-8'))
     print(csv_file)  # 可以先输出看一下该文件是什么样的类型
     content = []
